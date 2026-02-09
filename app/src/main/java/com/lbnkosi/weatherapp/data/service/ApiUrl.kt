1c514b647936fd1bc4ec74d5c1709d26d1226e90
Lebogang Nkosi <nkosil074@gmail.com>
2026-02-09 16:53:04 +0200

    Update ApiUrl.kt

--- app/src/main/java/com/lbnkosi/weatherapp/data/service/ApiUrl.kt
+++ app/src/main/java/com/lbnkosi/weatherapp/data/service/ApiUrl.kt
@@ -3,6 +3,6 @@ package com.lbnkosi.weatherapp.data.service
 object ApiUrl {
     const val BASE_URL = "https://api.openweathermap.org/"
     const val BASE_URL_NAME = "openweathermap_baseurl"
-    const val APP_ID = "" // TODO: APPID required to run
+    const val APP_ID = "b5af3cfe1a6a12c03adbd1f3565d20ef" // TODO: APPID required to run
     const val ONE_CALL_PATH = "data/3.0/onecall"
 }
